7267330ff925c567b665c930a9e56022b64eff4f
sickkside13 <sickksidex3@gmail.com>
2017-08-04 21:31:52 -0500

    clean up

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,8 +86,6 @@ BOARD_USES_QCNE := true
 
 # CPU
 ENABLE_CPUSETS := true
-ENABLE_SCHEDBOOST := true
-TARGET_USES_INTERACTION_BOOST := true
 
 # Pre-optimization
 ifneq ($(filter-out false,$(USE_DEXPREOPT)),)
